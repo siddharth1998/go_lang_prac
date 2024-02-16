ff5208dee2dcbfbbe722cb35287a4f4799aca964
siddharth1998 <siddharth1998@gmail.com>
2024-02-15 23:33:32 -0500

    added readme.md :: how to run the tcpProxy.go tool

--- tcpProxy/tcpProxy.go
+++ tcpProxy/tcpProxy.go
@@ -9,25 +9,30 @@ import (
 
 func handle(src net.Conn) {
 
-	dst, err := net.Dial("tcp", "google.com:443")
+	dst, err := net.Dial("tcp", "localhost:10000")
+
 	if err != nil {
 		log.Fatalln(err)
 		log.Fatalln("Problem in connecting with dst server")
 
 	}
+	
 	defer dst.Close()
 
 	go func() {
+
 		if _, err := io.Copy(src, dst); err != nil {
-		log.Fatalln(err)
+			log.Fatalln(err)
+	fmt.Printf("b")
 
 			log.Fatalln("Source not able to send data")
 		}
 	}()
 
+
 	if _, err := io.Copy(dst, src); err != nil {
 		log.Fatalln(err)
-
+		fmt.Printf("c")
 		log.Fatalln("dst not able to send the data")
 	}
 }
@@ -36,8 +41,9 @@ func main() {
 	fmt.Println("Starting the TCP server")
 	// accetp the connection
 	// send the data from the source to dst
-	listner, err := net.Listen("tcp", ":80	")
+	listner, err := net.Listen("tcp", ":80")
 	if err != nil {
+		log.Fatalln(err)
 		log.Fatalln("Problem starting the Proxy server (listner module)")
 
 	}
@@ -45,6 +51,8 @@ func main() {
 		conn, err := listner.Accept()
 		if err != nil {
 			log.Fatalln("Problem Accepting Connections")
+		}else{
+			fmt.Printf("New client connected")
 		}
 		// TODO send conn
 		go handle(conn)
