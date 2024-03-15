83423c08ee03361ba5271c9d0a888c08da60ffdb
siddharth1998 <siddharth1998@gmail.com>
2024-03-15 19:12:47 -0400

    added the structs for the code for the metasploit

---
+++ clientHTTP/metasploitRPC/Client/main.go
@@ -0,0 +1,20 @@
+package client
+
+type loginReq struct {
+	_msgpack struct{} `msgpack:,asArray`
+	Method   string
+	Username string
+	Password string
+}
+
+type loginRes struct {
+	Result       string `msgpack:"result"`
+	Token        string `msgpack:"token"`
+	Error        bool   `msgpack:"error"`
+	ErrorClass   string `msgpack:"error_class"`
+	ErrorMessage string `msgpack:"error_message"`
+}
+
+type logoutReq struct {
+	Result string `msgpack:"result"`
+}
